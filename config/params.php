35663dedaf40c1bf1b4e716f1432ba9128a223c1
wsydn <wsydney@web.de>
2023-01-13 21:15:59 +0100

    No need for params

--- config/params.php
+++ config/params.php
@@ -1,7 +1,5 @@
 <?php
 
 return [
-    'videoPath' => 'b:/Backups/WS4Media/Videos/art/',
-    'posterPath' => 'b:/Backups/WS4Media/videoposters/art/',
-    'thumbsPath' => 'b:/Backups/WS4Media/videothumbs/art/',
+
 ];
