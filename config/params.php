fc0e389d3e16f325f5d36179ea2dc69c15b874b8
wsydn <wsydney@web.de>
2023-01-13 16:46:32 +0100

    params

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
+    'videoPath' => 'b:/Backups/WS4Media/Videos/art/',
+    'posterPath' => 'b:/Backups/WS4Media/videoposters/art/',
+    'thumbsPath' => 'b:/Backups/WS4Media/videothumbs/art/',
 ];
--- models/Video.php
+++ models/Video.php
@@ -2,14 +2,11 @@
 
 namespace app\models;
 
+use Yii;
 use yii\db\ActiveRecord;
 
 class Video extends ActiveRecord
 {
-    public const ARTVIDEOS_VIDEOPATH = 'b:/Backups/WS4Media/Videos/art/';
-    public const ARTVIDEOS_POSTERPATH = 'b:/Backups/WS4Media/videoposters/art/';
-    public const ARTVIDEOS_THUMBSPATH = 'b:/Backups/WS4Media/videothumbs/art/';
-
     public static function tableName()
     {
         return 'art_videos';
@@ -17,7 +14,7 @@ class Video extends ActiveRecord
 
     public function getVideoPath()
     {
-        return self::ARTVIDEOS_VIDEOPATH . $this->folder . '/' . $this->filename;
+        return Yii::$app->params['videoPath'] . $this->folder . '/' . $this->filename;
     }
 
     public function getPosterPath()
@@ -27,7 +24,7 @@ class Video extends ActiveRecord
 
     public function getPosterFolderPath()
     {
-        return self::ARTVIDEOS_POSTERPATH . $this->folder;
+        return Yii::$app->params['posterPath'] . $this->folder;
     }
 
     public function getPosterFilename()
